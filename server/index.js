c3688ae5ce7ef522692a1808790c8ec2968f1dbe
Smruti Sagarika Swain <smruti.avanti@gmail.com>
2021-03-29 23:16:45 +0530

    gitignore update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use('/posts',postRoutes)
+app.use('/posts',postRoutes);
+
+app.get('/',(req,res)=>{
+    res.send('Hello to memories API');
+})
 
 const PORT = process.env.PORT || 5000;
 
